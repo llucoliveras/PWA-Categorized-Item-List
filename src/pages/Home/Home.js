e9ebf40eea97087e362bf1778fa6fd160bb0af26
llucoliveras <oliveraslluc@gmail.com>
2025-08-15 17:34:17 +0200

    HomePage added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,57 @@
+import { useEffect } from "react";
+import './Home.css'
+import Item from "./Item";
+import { useDataNavigatorContext } from "../../components/DataNavigatorContext";
+
 const Home = ({ savedUserLoginData }) => {
-    return <h1>Home</h1>
+    const {
+        path,
+        setPath,
+        currentList,
+        setCurrentList,
+        setPreviousList,
+    } = useDataNavigatorContext()
+
+    useEffect(() => {
+        const fetchData = async (fileName) => {
+            try {
+                const res = await fetch(`/data/${fileName}.json`);
+                if (!res.ok) throw new Error("File not found");
+                const data = await res.json();
+                setPath([data]);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        if (savedUserLoginData?.username) {
+            fetchData(savedUserLoginData.username);
+        }
+    }, [savedUserLoginData.username, setPath]);
+
+    useEffect(() => {
+        if (!path) return
+
+        if (path.length >= 1) {
+            setPreviousList(currentList)
+            setCurrentList(path[path.length - 1])
+        }
+    }, [path, setCurrentList])
+
+    const goDeeper = (node) => {
+        if (node.items) {
+            setPath(prev => [...prev, node])
+        }
+    }
+
+    return (
+        <div className="list-container">
+            {path == null && <div>LOADING DATA...</div>}
+            {path != null && currentList != null && currentList.items.map((node) => (
+                <Item key={node.id} onClick={() => goDeeper(node)} data={node} style={{ cursor: node.items ? 'pointer' : 'default' }}/>
+            ))}
+        </div>
+    )
 }
 
 export default Home;
